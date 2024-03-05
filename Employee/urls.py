db655a8316d22d01dff9e98cb20b52a7c3fb2bdd
shivaprasad0108 <shivaprasadnune2022@gmail.com>
2024-03-05 14:30:58 +0530

    edit

--- Employee/urls.py
+++ Employee/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/' , views.home, name='home'),
+    path('', views.home, name='home'),
     path('employees/',include('salary.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
